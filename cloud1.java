9f7d52b30afc3dcc4e8e1bb233937ce102b4bf2e
Your Name <you@example.com>
2019-10-14 21:43:42 -0700

    cloud4

--- cloud1.java
+++ cloud1.java
@@ -1,4 +1,7 @@
 public class cloud{
 System.out.println("from master branch");
 System.out.println("hi by master");
+
+sop("from cloud4 branch");
+sop("are yaar kitte baar hello bolu by cloud4");
 }
